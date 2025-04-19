def0db000482b0d39f439e76ff14c17e2a09c78e
Mudasir Sohail <159764920+mudasirsohail@users.noreply.github.com>
2025-04-19 12:27:43 +0500

    Update Countdown_Timer_Python_Project.py

--- Assignments_01_to_06/Countdown_Timer_Python_Project.py
+++ Assignments_01_to_06/Countdown_Timer_Python_Project.py
@@ -1,21 +1,3 @@
-# import time
-
-
-# def countdownTimer(t):
-#     while t:
-#         mins, secs = divmod(t , 60)
-#         timer = '{:02d}:{:02d}'.format(mins, secs)
-#         print(timer, end='\r')
-#         time.sleep(1)
-#         t -= 1
-
-#     print("Timer Completed!!")
-
-# t = input("Enter a time in Seconds?  ")
-# countdownTimer(int(t)) 
-
-
-
 import time
 
 def countdown_timer(t):
